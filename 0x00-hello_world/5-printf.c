dfcb88c13fedaaf64b6d819bd47d3bdf18e7771e
Hamza AJEM <75204256+HAJEM@users.noreply.github.com>
2023-03-18 19:35:35 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-f
+#include <stdio.h>
+/**
+*main - main entry point
+*return : always 0 (success)
+*/
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,\n);
+return (0);
+}
