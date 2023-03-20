43a811b414f457a4230661603291291b816e73e8
Hamza AJEM <75204256+HAJEM@users.noreply.github.com>
2023-03-20 23:51:57 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Always 0.
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int n;
+	int n, m;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
+	for (n = 48; n <= 56; n++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
-	}
-
+	putchar('\n');
 	return (0);
-
 }
