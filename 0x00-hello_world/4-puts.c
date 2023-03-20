1515adf692d4908fe3501c1278d8cc65d48336aa
Hamza AJEM <75204256+HAJEM@users.noreply.github.com>
2023-03-20 22:25:20 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*entry point main program*/
+ /**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
